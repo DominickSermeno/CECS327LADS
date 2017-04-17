9036d52c1d21c8cdb18d3557d103f69ac235e1c0
TheDomple <dominick.sermeno@gmail.com>
2017-04-17 13:31:17 -0700

    Add files via upload

---
+++ CECS327TeamLADS/src/lab7/TCPstreamSocketServer.java
@@ -0,0 +1,37 @@
+package lab7;
+
+
+
+public class TCPstreamSocketServer 
+{
+	public static void main(String[] args) 
+	{
+        
+       /*
+        *Server code goes here  
+        * 
+        **/ 
+		
+	} // end of main 
+	
+	
+	//Dominick
+	public double nextEvenFib()
+	{
+		return 0.0;
+	}
+	//Lori
+	public int nextLargerRand() 
+	{
+		return 0;
+	}
+	//Steph
+	public int nextPrime()
+	{
+		return 0;
+	}
+} // end of server program 
+
+
+	
+

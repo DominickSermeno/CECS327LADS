fe14e7c39c6d67a8194fcf2c695618c14bb29df1
TheDomple <dominick.sermeno@gmail.com>
2017-04-25 16:50:53 -0700

    Revert "Add files via upload"
    This reverts commit 760ec05dba706bdc1a934a14ec8494c3e9a77958.

--- TCPstreamSocketServer.java
+++ TCPstreamSocketServer.java
@@ -16,19 +16,67 @@ public class TCPstreamSocketServer
 	
 	
 	//Dominick
-	public double nextEvenFib()
-	{
-		return 0.0;
-	}
+	  public static long fibonacci(long n) 
+	  {
+	        if (n <= 1)
+	        {
+	        	//do nothing+
+	        }
+	        else
+	        {
+	        	n = fibonacci(n-1) + fibonacci(n-2);
+	        }
+	        return n;
+	    }
+	  
+	  public static void nextEvenFib(long n)
+	  {
+		  long temp = 1;
+		  long evens[]= new long[5];
+		  int i = 0;
+		  for(int ii = 0; ii < 5; ii++)
+		  {
+			  while(temp%2!=0)
+			  {
+				  temp = fibonacci(n+i);
+				  i++;
+			  }
+			  evens[ii] = temp;
+			  temp++;
+			  i++;
+		  }  
+		  System.out.println("The Next Five Even Fibonacci Values are: " );
+		  for (int iii = 0; iii < 5; iii++)
+		  {
+			  System.out.println(evens[iii]);
+		  }
+		  
+	  }
 	//Lori
 	public int nextLargerRand() 
 	{
 		return 0;
 	}
 	//Steph
-	public int nextPrime()
-	{
-		return 0;
+	public int nextPrime(int prime){
+		boolean isPrime = false;
+
+	    int start = 2;
+
+	    while (!isPrime) {
+	    	prime += 1;
+	        int m = (int) Math.ceil(Math.sqrt(prime));
+
+	        isPrime = true;
+	        for (int i = start; i <= m; i++) {
+	            if (prime % i == 0) {
+	                isPrime = false;
+	                break;
+	            } 
+	        }
+	    }
+	    return prime;
+	
 	}
 } // end of server program 
 
